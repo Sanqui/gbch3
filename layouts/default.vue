a9bb0ac9ac71d4d3695b7ece68ff9d18651acbc8
Sanqui <me@sanqui.net>
2020-08-16 15:22:13 +0200

    Turn into a Nuxt.js project

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>My First Nuxt.js Website!</h1>
+    <div>
+      <Nuxt />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h2>Home page</h2>
+    <NuxtLink to="/about">
+      About
+    </NuxtLink>
+    <br>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
