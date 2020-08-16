af1651767cb90b65114d0e790abe37c3b8e92956
Sanqui <me@sanqui.net>
2020-08-16 15:54:55 +0200

    Show list of games

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
   <main>
-    <h2>Home page</h2>
-    <NuxtLink to="/about">
+    <h2>Games</h2>
+    <!--
+    <nuxt-link to="/about">
       About
-    </NuxtLink>
+    </nuxt-link>
     <br>
     <a href="https://nuxtjs.org">External Link to another page</a>
+    -->
+    <ul>
+      <li v-for="(game, key) in data" :key="key">{{ key }}</li>
+    </ul>
   </main>
 </template>
+
+<script>
+import data from '~/data/data.json'
+
+export default {
+  asyncData () {
+    return { data: data }
+  }
+}
+</script>
