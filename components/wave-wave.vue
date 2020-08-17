823a36e6048490e5fb0f9cc1e59be3ed1c1a03b8
Jiří Šebele <sindriava@gmail.com>
2020-08-17 16:07:49 +0200

    Add waveform rendering

--- components/wave-wave.vue
+++ components/wave-wave.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wave">
     <div class="left">
-      <tt>{{ wave }}</tt>
+      <svg viewBox="0 0 1 1" preserveAspectRatio="none">
+        <polygon :points="wavePoints" />
+      </svg>
+      <tt ref="code">{{ wave }}</tt>
       <!-- TODO copy as ascii button -->
     </div>
     <div>
@@ -23,25 +26,60 @@
 
   export default {
     props: ['wave_data', 'wave'],
+    computed: {
+      wavePoints() {
+        var points = []
+        points.push("0,1")
+        for (var n = 0; n < 32; n++) {
+          var amplitude = parseInt(this.wave[n], 16);
+          var point = { x: (n) / 32.0, y: 1.0 - amplitude / 16.0 };
+          points.push(point.x + "," + point.y)
+          points.push((n+1) / 32.0 + "," + point.y)
+        }
+        points.push("1,1")
+        return points.join(" ");
+      }
+    },
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .wave {
     display: flex;
     border: 1px solid #999;
     padding: 4px;
     margin: 4px;
-  }
-  
-  .wave .left {
-    font-size: 150%;
-    margin: auto 12px;
-  }
-  .wave > div {
-    margin: 2px 12px;
-  }
-  .wave button {
-    height: 100%;
+    
+    .left {
+      font-size: 150%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px;
+
+      svg {
+        display: block;
+        width: 100%;
+        height: 128px;
+        flex-grow: -1;
+        flex-shrink: 10;
+
+        polygon {
+          fill: rgba(#0000ff, 0.5);
+        }
+      }
+
+      tt {
+        display: block;
+      }
+    }
+    
+    > div {
+      margin: 2px 12px;
+    }
+
+    button {
+      height: 100%;
+    }
   }
 </style>
